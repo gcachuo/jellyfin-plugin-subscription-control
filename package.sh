eeafb6fbc7eadc926d5548ebab72428d980455a6
Memo Cachú <gcachu.o@gmail.com>
2026-07-10 13:03:55 -0600

    Release v1.0.11.1 - Grant full access to trial users without plan. Users without a plan now get EnableAllFolders=true and LiveTV=true to evaluate the service. Restrictions only apply when they subscribe to a specific plan.

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.0.11.0"
+VERSION="1.0.11.1"
 BUILD_DIR="EasyMovie.Plugin/bin/Release/net9.0"
 PACKAGE_NAME="EasyMovie.Plugin-${VERSION}.zip"
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%S.0000000Z")
